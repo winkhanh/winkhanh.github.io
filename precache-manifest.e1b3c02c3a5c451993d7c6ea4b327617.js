2b6976814cb7955cdc17b2e54ef6cb1496ddbb88
winkhanh <giakhanhnk2000@gmail.com>
2020-08-06 01:44:19 -0400

    Updates

---
+++ precache-manifest.e1b3c02c3a5c451993d7c6ea4b327617.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53361f8329edd0fa64dcf02bdee37d54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff5e935e0512ac88dea4",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b8dbcd1c1e7e4e014350",
+    "url": "/static/css/main.9ca6432f.chunk.css"
+  },
+  {
+    "revision": "ff5e935e0512ac88dea4",
+    "url": "/static/js/2.134b9e8c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.134b9e8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8dbcd1c1e7e4e014350",
+    "url": "/static/js/main.f3798501.chunk.js"
+  },
+  {
+    "revision": "8cdf164702af695058ba",
+    "url": "/static/js/runtime-main.223301a1.js"
+  },
+  {
+    "revision": "055d896f91d3d042dfbf76d180a7ffb9",
+    "url": "/static/media/Bg1.055d896f.svg"
+  },
+  {
+    "revision": "92e34efc97b06a939c7fd7e8fd622e6d",
+    "url": "/static/media/Bg2.92e34efc.svg"
+  },
+  {
+    "revision": "d113c5115631f53bbff3f487282e9242",
+    "url": "/static/media/Bg3.d113c511.svg"
+  },
+  {
+    "revision": "43910bb44108468e55f751a2dcf4634e",
+    "url": "/static/media/DiscordWin.43910bb4.svg"
+  },
+  {
+    "revision": "1049d0caa1dcf25c88e9af524b761dad",
+    "url": "/static/media/FacebookWin.1049d0ca.svg"
+  },
+  {
+    "revision": "c6f31b316a2d0c8f3dc4c4ef8f8afb06",
+    "url": "/static/media/GitWin.c6f31b31.svg"
+  },
+  {
+    "revision": "756916bf5ebb25d9902f8096b4b07b2c",
+    "url": "/static/media/InstagramWin.756916bf.svg"
+  },
+  {
+    "revision": "62062077c63196a629201370456d1312",
+    "url": "/static/media/me.62062077.jpg"
+  },
+  {
+    "revision": "0e3f0b9faae784cd8199696a460f647d",
+    "url": "/static/media/p1_bg.0e3f0b9f.png"
+  }
+]);
--- src/context/theme.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ThemeContext= React.createContext({
-    theme:0,
-    setTheme:(aNum:number)=>{},
-    changeTheme:()=>{}
-});
-
-export default ThemeContext;
